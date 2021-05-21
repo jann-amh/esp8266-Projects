a100d6094080140a8950976e676b7a4adaef799a
LAPTOP-4E5S1PBM\janna <j.amhausend@gmail.com>
2021-05-21 11:10:28 +0200

    initial commit

---
+++ CaptivePortal.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include "index.h"
+
+const byte DNS_PORT = 53;
+IPAddress apIP(172, 217, 28, 1);
+DNSServer dnsServer;
+ESP8266WebServer webServer(80);
+
+String responseHTML = ""
+                      "<!DOCTYPE html><html lang='en'><head>"
+                      "<meta name='viewport' content='width=device-width'>"
+                      "<title>CaptivePortal</title></head><body>"
+                      "<h1>Bruce U VON</h1></body></html>";
+
+void setup() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("If u read this, u lost!", "ViVonZULUL");
+  
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  webServer.onNotFound([]() {
+    webServer.send(200, "text/html", responseHTML);
+  });
+  webServer.begin();
+}
+
+void loop() {
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+}
